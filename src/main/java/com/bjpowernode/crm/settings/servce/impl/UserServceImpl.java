b1fc2f6dc53a076dc0b9076e1619da0ff69b04de
zs <224@qq.com>
2021-08-17 12:52:42 +0800

    2

--- src/main/java/com/bjpowernode/crm/settings/servce/impl/UserServceImpl.java
+++ src/main/java/com/bjpowernode/crm/settings/servce/impl/UserServceImpl.java
@@ -2,8 +2,8 @@ package com.bjpowernode.crm.settings.servce.impl;
 
 import com.bjpowernode.crm.settings.dao.UserDao;
 import com.bjpowernode.crm.settings.servce.UserServce;
-import com.bjpowernode.crm.untils.MyBatisUntil;
+import com.bjpowernode.crm.untils.SqlSessionUtil;
 
 public class UserServceImpl implements UserServce {
-    private UserDao userDao = MyBatisUntil.getSqlSession().getMapper(UserDao.class);
+    private UserDao userDao = SqlSessionUtil.getSqlSession().getMapper(UserDao.class);
 }
